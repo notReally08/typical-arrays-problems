9cd95e35910caf12b448b14dc78abdbc4f625b81
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-10 14:51:57 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  
+  let minNum = array[0];
+  array.forEach(num => {
+    if(num < minNum) {
+      minNum = num
+    }
+  })
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  
+  let maxNum = array[0];
+  array.forEach(num => {
+    if(num > maxNum) {
+      maxNum = num
+    }
+  })
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  
+  let res = 0;
+  let sum = array.reduce((accumulator, currentValue) => accumulator + currentValue)
+  res = sum / array.length
+  return res;
 }
